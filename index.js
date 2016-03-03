390208da24c24f217d04dee0a710476eb035dbe3
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-03 12:30:26 -0700

    finished in class/lect backedn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var BookController = require('./controllers/BookController');
+
 var app = express();
 
 app.use(bodyParser.json());
 
-
-
-
-
-
+app.get('/books', BookController.index);
+app.get('/books/:idx', BookController.show);
+app.put('/books/:idx', BookController.update);
+app.post('/books', BookController.create);
+app.delete('/books/:idx', BookController.destroy);
 
 var port = 3000;
 app.listen(port, function() {
---
+++ models/Books.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    title: 'Mist Born',
+    author: 'Brennan',
+    rating: 3
+  }, {
+    title: 'Rainbow 6',
+    author: 'Tom Clancey',
+    rating: 6
+  }, {
+    title: 'Everyone does numba 2',
+    author: 'Sanitation Work #5',
+    rating: 2
+  }
+]
